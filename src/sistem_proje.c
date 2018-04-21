31709006facf642fb5e629db20c247f20f445c4b
Kielder <mcyurga2@gmail.com>
2018-04-21 18:32:44 +0300

    final

--- src/sistem_proje.c
+++ src/sistem_proje.c
@@ -1,28 +1,3 @@
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include "jrb.h"
-// #include "fields.h"
-
-// int main(int argc, char *argv[]) {
-//     IS wr;
-//     //wr = new_inputstruct("test.txt");
-
-//     FILE *fp;
-//     fp = fopen("test.txt", "w+");
-
-//     char buf[50];
-//     strcpy(buf, "icerik");
-
-//     if(fp != NULL)
-//         fprintf(fp, "Agac dosyaya yazildi!\n");
-//     else
-//         printf("NULL!\n");
-
-//     fclose(fp);
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,11 +14,33 @@ int main(int argc, char *argv[]) {
     char    temp[100];
 
     redBlackTree = make_jrb();
+
     is = new_inputstruct(NULL); // use NULL for std input
-    
+    wr = new_inputstruct(argv[1]);
+
+    if(wr == NULL) {
+        printf("ERROR: Verilen dosya ismi hatali.\n");
+    }
+    else if(argc == 2) { // ./program fileName şeklinde başlatıldığında .txt içindeki verileri ağaca ekler.
+        while(get_line(wr) != EOF) {
+            if(!strcmp(wr->text2,"add") && wr->NF == 4) {
+                strcpy(temp, wr->fields[1]);
+                strcat(temp, ",");
+                strcat(temp, wr->fields[2]);
+                strcat(temp, ",");
+                strcat(temp, wr->fields[3]);
+                jrb_insert_str(redBlackTree, strdup(wr->fields[0]), new_jval_s(strdup(temp)));
+            }
+            else {
+                printf("ERROR: Bicim olarak uygun olmayan kayita rastlandi. Agaca eklenmedi.\n");
+            }
+        }
+    }
+
     while(strcmp(is->text2, "quit\n")) {
         printf("> ");
         fflush(stdout);
+
         while(get_line(is) >= 0) {
             if(!strcmp(is->text2, "add")) { 
                 if(is->NF < 4 || is->NF > 4) {
@@ -58,8 +55,6 @@ int main(int argc, char *argv[]) {
                         strcat(params, ",");
                         strcat(params, is->fields[3]);
                         jrb_insert_str(redBlackTree, strdup(is->fields[0]), new_jval_s(strdup(params)));
-                        // Ağaca eklemede bir sıkıntı yok. Örnek eklenen: add|mc|a|b|c
-                        // Çıktı ----> sipKey: add ve Valuelar: mc,a,b,c
                     }
                     else
                         printf("ERROR: Eklemek istediginiz key degeri agac icerisinde mevcut.\n");
@@ -70,6 +65,7 @@ int main(int argc, char *argv[]) {
                     printf("ERROR: 1 adet parametre girmelisiniz.\n");
                 }
                 else {
+                    memset(temp,0,sizeof(temp));
                     jrb_traverse(rDugum, redBlackTree) {
                         strcpy(temp, rDugum->key.s); // Örn: "search|a" diye 'a' anahtarını arattığımızda
                         strcat(temp, "\n"); // fields[0] = "a\n" olarak atandığından rDugum sonuna strcat ile "\n" ekle
@@ -90,37 +86,35 @@ int main(int argc, char *argv[]) {
                 }
                 else {
                     int length = strlen(is->fields[0]) - 1;
-                    strncpy(temp, is->fields[0], length); // dosya ismini düzgün şekilde almak için (çalışıyor)
-                    fp = fopen(temp, "w+"); // parametre olarak verilen dosyayı yazmak için aç (yoksa oluşturur)
+                    memset(temp,0,sizeof(temp));
+                    strncpy(temp, is->fields[0], length);
+                    fp = fopen(temp, "w+");
 
-                    printf("Dosya ismi (temp): %s\n", temp);
                     if(fp == NULL) 
                         printf("ERROR: Dosya okuma/yazma hatası!\n");
                     else {
+                        sortedTree = make_jrb();            
                         jrb_traverse(rDugum, redBlackTree) {
+                            jrb_insert_gen(sortedTree, rDugum->key, rDugum->val, karsilastir);
+                        }
+                        jrb_traverse(rDugum, sortedTree) {
                             strcpy(temp, rDugum->key.s);
                             strcat(temp, ",");
                             strcat(temp, rDugum->val.s); // temp içinde örneğin: "07,iPhone,Metal,White" var artık
-                            fprintf(fp, "%s", temp);   // açtığımız dosya içerisine 'temp' değerini yaz.
-                            // printf(" temp son hali: %s", temp);
-                            //printf("%s,%s" , rDugum->key.s, rDugum->val.s);
+                            fprintf(fp, "%s", temp);   // açtığımız dosya içerisine 'temp' değerini yaz. 
                         }
+                        fclose(fp);
                     }
                 }
             }
-            else if (!strncmp(is->text2, "print", 5)) { // EKSIK. Key numarasının büyüklüğüne göre sıralanmalı
-                sortedTree = make_jrb();
+            else if (!strncmp(is->text2, "print", 5)) {   
+                sortedTree = make_jrb();            
                 jrb_traverse(rDugum, redBlackTree) {
-                    jrb_insert_gen(sortedTree, rDugum->val, JNULL, karsilastir);
+                   jrb_insert_gen(sortedTree, rDugum->key, rDugum->val, karsilastir);
                 }
 
-                jrb_rtraverse(rDugum, sortedTree) {
-                    //g = (Golfer *) rnode->key.v;
-                    printf("key.s: %s ve val.s %s\n", rDugum->key.s, rDugum->val.s);
-                    // dll_traverse(rDugum, g->scores) {
-                    //     s = (Score *) dnode->val.v;
-                    //     printf("  %3d : %s\n", s->score, s->tname);
-                    // }
+                jrb_traverse(rDugum, sortedTree) {
+                    printf("%s,%s", rDugum->key.s, rDugum->val.s);
                 }
             }
             else if(strncmp(is->text2, "quit", 4)){
@@ -129,20 +123,34 @@ int main(int argc, char *argv[]) {
 
             break;
         }
-        // jrb_traverse(rDugum, redBlackTree) {
-        //         printf("%s,%s" , rDugum->key.s, rDugum->val.s);
-        // }
     }//WHILE() BITISI
 
+    // Temizleme
+    jrb_traverse(rDugum, redBlackTree) {
+        if(rDugum->flink != NULL)
+            free(jval_s(rDugum->key));
+    }
+    if(sortedTree != NULL) {
+        jrb_traverse(rDugum, sortedTree) {
+            if(rDugum->flink != NULL)
+                free(jval_s(rDugum->key));
+        }
+    }
     jrb_free_tree(redBlackTree);
+    if(sortedTree != NULL) {
+        jrb_free_tree(sortedTree);
+    }
     jettison_inputstruct(is);
+    jettison_inputstruct(wr);
+
     return 0;
 }
 
 int karsilastir(Jval d1, Jval d2) {
-    if(d1.s < d2.s) 
+    if(atoi(d1.s) > atoi(d2.s)) {
         return 1;
-    if(d1.s > d2.s)
+    }
+    if(atoi(d1.s) < atoi(d2.s))
         return -1;
     else 
         return 0;
